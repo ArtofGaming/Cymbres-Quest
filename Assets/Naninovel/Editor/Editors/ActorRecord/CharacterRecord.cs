1239db8b3b2e3c6b412b1fd3ad4904a9d822570b
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2022-09-28 12:06:51 -0700

    naninovel is out YAML is in

--- Assets/Naninovel/Editor/Editors/ActorRecord/CharacterRecord.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2021 Elringus (Artyom Sovetnikov). All rights reserved.
-
-using UnityEngine;
-
-namespace Naninovel
-{
-    [CreateAssetMenu(menuName = "Naninovel/Actor Record/Character", fileName = "NewCharacterId")]
-    public class CharacterRecord : ActorRecord<CharacterMetadata> { }
-}
--- Assets/Naninovel/Editor/Editors/ActorRecord/ChoiceHandlerRecord.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2021 Elringus (Artyom Sovetnikov). All rights reserved.
-
-using UnityEngine;
-
-namespace Naninovel
-{
-    [CreateAssetMenu(menuName = "Naninovel/Actor Record/Choice Handler", fileName = "NewChoiceHandlerId")]
-    public class ChoiceHandlerRecord : ActorRecord<ChoiceHandlerMetadata> { }
-}
