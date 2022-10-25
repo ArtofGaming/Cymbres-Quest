53f7ba706518610bc9a3289c540521e36df7f718
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-10-25 12:51:21 -0700

    Implemented Buttons to Start Screen
     - New game Button goes to next scene
     - Load button & Exit Button are debugs
     - Options open temp panel
     - Changed Build scene order to startscene = 0

--- Assets/Scripts/StartSceneFunction.cs
+++ Assets/Scripts/StartSceneFunction.cs
@@ -1,21 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //
 public class StartSceneFunction : MonoBehaviour
 {
-    #region "vars"
-    int x;
-    char y;
-    string z;
-    GameManager manager;
-    #endregion
+    private bool OptionIsOn = false;
+    public GameObject OptionsPanel;
 
-    
+    public void NewGamePressed()
+    {
+        Debug.Log("New Game to be started.");
+        SceneManager.LoadScene("Customization");
+    }
+
+    public void LoadGamePressed()
+    {
+        Debug.Log("This has not been implemented yet.");
+    }
+
+    public void OptionsToggle()
+    {
+        if(OptionIsOn == false)
+        {
+            OptionIsOn = true;
+            OptionsPanel.SetActive(true);
+            Debug.Log("Options are now to be shown");
+        }
+        else if(OptionIsOn == true)
+        {
+            OptionIsOn=false;
+            OptionsPanel.SetActive(false);  
+            Debug.Log("Options are now turned off");
+        }
+
+    }
     public void ExitGame()
     {
-
+        Debug.Log("Game is exiting now.");
         Application.Quit();
     }
 }
