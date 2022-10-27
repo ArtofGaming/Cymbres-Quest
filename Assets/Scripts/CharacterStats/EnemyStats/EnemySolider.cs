84b91dfcbc4d1cea3e86788ebafbaafd52b156b4
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2022-10-27 14:23:03 -0700

    working on stat scripts

--- Assets/Scripts/CharacterStats/EnemyStats/EnemySolider.cs
+++ Assets/Scripts/CharacterStats/EnemyStats/EnemySolider.cs
@@ -14,14 +14,14 @@ public class EnemySolider : UnitInfo
         }
         unitMaxHealth = unitLevel * 3 + 12;
         unitHealth = unitMaxHealth;
-        unitMovementSpeed = unitLevel * .15f - .05f;
-        unitAttackRange = unitLevel * .15f - .05f;
+        unitMovementSpeed = .1f;
+        unitAttackRange = .1f;
         unitAttack = Mathf.RoundToInt(unitLevel * 1.15f + 7);
-        unitCritChance = Mathf.RoundToInt(unitLevel * .15f + 4);
-        unitSkillResist = Mathf.RoundToInt(unitLevel * .5f + 4);
-        unitDefense = Mathf.RoundToInt(unitLevel * 1.15f + 5);
-        unitEnergy = Mathf.RoundToInt(unitLevel * .75f + 2);
-        unitEvasion = Mathf.RoundToInt(unitLevel * .3f + 3);
+        unitCritChance = 0;
+        unitSkillResist = Mathf.RoundToInt(unitLevel * .5f + 1);
+        unitDefense = Mathf.RoundToInt(unitLevel * 1.15f + 2);
+        unitEnergy = 0;
+        unitEvasion = 0;
 
     }
 
