e81234c4a8d6a27776d716660c713995e37ff666
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-10-28 11:07:46 -0700

    Text Controller Generally completed
    - formatted the text to work with the code
    - Introduction file is ready to go
    - Awaiting approval to continue completing this controller

--- Assets/Scripts/TextBoxControllerScript.cs
+++ Assets/Scripts/TextBoxControllerScript.cs
@@ -3,19 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro; // for TMP text UI
 using System.IO; //File IO for text file scripts
+using UnityEngine.UI;
 
 public class TextBoxControllerScript : MonoBehaviour
 {
     #region "Vars"
-    public TextMeshProUGUI TextMesh;
+    public TextMeshProUGUI DialogueTextMesh;
+    public TextMeshProUGUI CharNameTextMesh;
     public TextAsset TextFile;
+    public float TextTimeDelay = .125f;
+    public Button NextTextButton;
+    //DO we need a button to go to the next scene?
+    //public Button CloseUIButton;
+    private string CharacterName;
+    private string DialogueText;
+    private StreamReader textReader;
+
 
     #endregion
 
-    //Create a file IO that will grab the correct text script for items.
+    private void Start()
+    {
+        StartText();
+    }
 
-    //Connect and fix the text script to work with the text file and read it correctly
+    public void StartText()
+    {
+        string path = "Assets/Resources/Narrative/" + TextFile.name +".txt";
+        //Read the text from directly from the test.txt file
+        textReader = new StreamReader(path);
 
-    //CharacterName "textstuff" will be the format to read in the files
+        //textReader = new StreamReader(Path.Combine(Application.streamingAssetsPath, TextFile.name));
+        GrabNextText();
+    }
 
+    public void GrabNextText()
+    {
+        NextTextButton.gameObject.SetActive(false);
+
+        if (textReader != null && !textReader.EndOfStream)
+        {
+
+            CharacterName = textReader.ReadLine();
+            DialogueText = textReader.ReadLine();
+
+            CharNameTextMesh.text = CharacterName;
+            DialogueTextMesh.text = "";
+
+            StartCoroutine(PlayText());
+        }
+        else
+        {
+            EndText();
+        }
+
+
+    }
+
+    public void EndText()
+    {
+        textReader.Close();
+
+    }
+
+        IEnumerator PlayText()
+    {
+
+        foreach(char c in DialogueText)
+        {
+            DialogueTextMesh.text += c;
+            yield return new WaitForSeconds(TextTimeDelay);
+        }
+
+        NextTextButton.gameObject.SetActive(true);
+    }
 }
