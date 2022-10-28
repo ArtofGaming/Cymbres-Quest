9ce07e41dc0341a23833e6b8f48f0abfdc7a8f41
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-10-28 09:58:43 -0700

    In progress of textbox
    - started code for textbox
    - creating text asset file set up for code to read
    - imported textbox art assets

---
+++ Assets/Scripts/TextBoxControllerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // for TMP text UI
+using System.IO; //File IO for text file scripts
+
+public class TextBoxControllerScript : MonoBehaviour
+{
+    #region "Vars"
+    public TextMeshProUGUI TextMesh;
+    public TextAsset TextFile;
+
+    #endregion
+
+    //Create a file IO that will grab the correct text script for items.
+
+    //Connect and fix the text script to work with the text file and read it correctly
+
+    //CharacterName "textstuff" will be the format to read in the files
+
+}
