cd7dbc406bdb68d1a8654d3ec54236895fc34298
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-11-21 16:31:24 -0700

    new destroy on load

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using System.Linq;
 
+
 public class GameManager : MonoBehaviour
 {
     #region "Variables in GameManager"
@@ -45,28 +46,7 @@ public class GameManager : MonoBehaviour
         attackingUnit = null;
         whoseTurn = "Player";
 
-
-        #region "Grabbing player units position and set active; grabs enemies units"
-        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Player").Length; i++)
-        {
-            //Debug.Log(GameObject.FindGameObjectsWithTag("Spawn").Length);
-            
-        }
-        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Player"))
-        {
-            aliveUnits.Add(unit);
-            //unit.gameObject.transform.localPosition = GameObject.FindGameObjectsWithTag("Spawn")[aliveUnits.IndexOf(unit)].gameObject.transform.localPosition;
-            //Debug.Log(unit.transform.childCount);
-            //unit.transform.GetChild(0).Set
-        }
-        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Enemy"))
-        {
-            
-            aliveEnemyUnits.Add(unit);
-
-        }
-
-        #endregion
+        DontDestroyOnLoad(this.gameObject);
 
         TurnSwitch();
 
