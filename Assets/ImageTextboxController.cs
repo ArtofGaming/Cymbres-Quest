4561c11c18f12b0e17035c356b28aeb3082bb23d
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-12-05 15:11:08 -0700

    Fixed bug in Image script

--- Assets/ImageTextboxController.cs
+++ Assets/ImageTextboxController.cs
@@ -8,14 +8,28 @@ public class ImageTextboxController : MonoBehaviour
     public List<Sprite> PortraitSequence;
     public Image ImageGO;
     public int ImageCounter = 0;
+    private int ListCount;
 
+    private void Start()
+    {
+        ListCount = PortraitSequence.Count;
+    }
     public void ChangeImage()
     {
+        
         ImageCounter++;
+        //If Image counter goes past the index limit of the list, return.
+        if (ImageCounter+1 >= PortraitSequence.Count)
+        {
+            Debug.Log("The PortraitSequence requires more elements");
+            return;
+        }
+
+        //
         if (PortraitSequence != null && PortraitSequence[ImageCounter] != null)
-        {   
-            ImageGO.gameObject.SetActive(true);
-            ImageGO.sprite = PortraitSequence[ImageCounter];
+        {
+                ImageGO.gameObject.SetActive(true);
+                ImageGO.sprite = PortraitSequence[ImageCounter];
         }
         else
         {
