3ca06f751423df3f453a65e9ee6020d3a4f12da2
ArtofGaming <turner4965@gmail.com>
2023-09-14 22:15:42 -0700

    test

--- Assets/Naninovel/Editor/Editors/ActorMetadata/ChoiceHandlerMetadataEditor.cs
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2021 Elringus (Artyom Sovetnikov). All rights reserved.
-
-namespace Naninovel
-{
-    public class ChoiceHandlerMetadataEditor : MetadataEditor<IChoiceHandlerActor, ChoiceHandlerMetadata> { }
-}
